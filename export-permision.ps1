02d94d32906d1cbf4ff258b3995d7664f96b60a4
pablomadriaga <75373441+pablomadriaga@users.noreply.github.com>
2021-03-01 17:27:36 -0300

    v1

---
+++ export-permision.ps1
@@ -0,0 +1,64 @@
+﻿#Get the Permissions 
+
+
+$sourceVI = Read-Host -Prompt "Incregre el Vcenter"
+
+
+#Conectarse al Vcenter
+try {
+    Connect-VIServer $sourceVI -ErrorAction Stop | Out-Null
+}#Fin del Try
+catch {
+    Write-Host "No se puede conectar con el servidor" -ForegroundColor Yellow
+    Break
+}#Fin del Catch
+ 
+
+$datacenter = Read-Host -Prompt "Incregre el Datacenter"
+
+$folderperms = get-datacenter $datacenter -Server $sourceVI | Get-Folder | Get-VIPermission
+$vmperms = Get-Datacenter $datacenter -Server $sourceVI | get-vm | Get-VIPermission
+
+$permissions = get-datacenter $datacenter -Server $sourceVI | Get-VIpermission
+
+        $report = @()
+            foreach($perm in $permissions){
+               $row = "" | select EntityId, FolderName, Role, Principal, IsGroup, Propagate
+               $row.EntityId = $perm.EntityId
+               $Foldername = (Get-View -id $perm.EntityId).Name
+               $row.FolderName = $foldername
+               $row.Principal = $perm.Principal
+               $row.Role = $perm.Role
+               $row.IsGroup = $perm.IsGroup
+               $row.Propagate = $perm.Propagate
+               $report += $row
+            }
+
+            foreach($perm in $folderperms){
+               $row = "" | select EntityId, FolderName, Role, Principal, IsGroup, Propagate
+               $row.EntityId = $perm.EntityId
+               $Foldername = (Get-View -id $perm.EntityId).Name
+               $row.FolderName = $foldername
+               $row.Principal = $perm.Principal
+               $row.Role = $perm.Role
+               $row.IsGroup = $perm.IsGroup
+               $row.Propagate = $perm.Propagate
+               $report += $row
+            }
+
+            foreach($perm in $vmperms){
+                $row = "" | select EntityId, FolderName, Role, Principal, IsGroup, Propagate
+                $row.EntityId = $perm.EntityId
+                $Foldername = (Get-View -id $perm.EntityId).Name
+                $row.FolderName = $foldername
+                $row.Principal = $perm.Principal
+                $row.Role = $perm.Role
+                $row.IsGroup = $perm.IsGroup
+                $row.Propagate = $perm.Propagate
+                $report += $row
+            }
+
+        $report | export-csv "c:\perms-$($datacenter).csv" -NoTypeInformation
+
+
+Disconnect-VIServer * -Force -Confirm:$false
---
+++ import-permision.ps1
@@ -0,0 +1,59 @@
+﻿#Get the Permissions 
+
+
+$VI = Read-Host -Prompt "Incregre el Vcenter"
+
+
+#Conectarse al Vcenter
+try {
+    Connect-VIServer $VI -ErrorAction Stop | Out-Null
+}#Fin del Try
+catch {
+    Write-Host "No se puede conectar con el servidor" -ForegroundColor Yellow
+    Break
+}#Fin del Catch
+ 
+
+$datacenter = Read-Host -Prompt "Incregre el Datacenter"
+
+
+
+
+
+$permissions = @()
+$permissions = Import-Csv "c:\perms-$($datacenter).csv"
+
+foreach ($perm in $permissions) {
+    $entity = ""
+    $entity = New-Object VMware.Vim.ManagedObjectReference
+
+    switch -wildcard ($perm.EntityId)
+        {
+            Folder* {
+            $entity.type = "Folder"
+            $entity.value = ((get-datacenter $datacenter | get-folder $perm.Foldername).ID).Trimstart("Folder-")
+        }
+            VirtualMachine* {
+            $entity.Type = "VirtualMachine"
+            $entity.value = ((get-datacenter $datacenter | Get-vm $perm.Foldername).Id).Trimstart("VirtualMachine-")
+        }
+}
+$setperm = New-Object VMware.Vim.Permission
+$setperm.principal = $perm.Principal
+    if ($perm.isgroup -eq "True") {
+        $setperm.group = $true
+    } else {
+        $setperm.group = $false
+    }
+$setperm.roleId = (Get-virole $perm.Role).id
+    if ($perm.propagate -eq "True") {
+        $setperm.propagate = $true
+    } else {
+        $setperm.propagate = $false
+    }
+
+$doactual = Get-View -Id 'AuthorizationManager-AuthorizationManager'
+$doactual.SetEntityPermissions($entity, $setperm)
+}
+
+Disconnect-VIServer * -Force -Confirm:$false
